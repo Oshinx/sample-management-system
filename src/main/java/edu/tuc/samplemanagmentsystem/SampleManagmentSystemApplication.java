ebdfbeceb574e62dd4ae27621d023109a782458e
Michael Oshinaike <michaeloshinaike@gmail.com>
2023-12-26 05:33:48 +0100

    project setup

---
+++ src/main/java/edu/tuc/samplemanagmentsystem/SampleManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.tuc.samplemanagmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleManagmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleManagmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/tuc/samplemanagmentsystem/SampleManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tuc.samplemanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
